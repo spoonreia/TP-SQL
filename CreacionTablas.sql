878e3d71e8f3ad54d00bbfdf8afd06bac7bf94dd
Gonzalo Casas <gonzaloacasas@gmail.com>
2024-10-26 00:14:57 -0300

    Termine de crear todas las tablas

--- CreacionTablas.sql
+++ CreacionTablas.sql
@@ -4,6 +4,22 @@ GO
 -- Creación de las tablas especificadas en el DER
 -- ==============================================
 
+IF EXISTS(SELECT 1 FROM SYS.all_objects WHERE type = 'U' AND object_id = OBJECT_ID('[dbAuroraSA].[SucursalProducto]'))
+	DROP TABLE dbAuroraSA.SucursalProducto;
+GO
+
+IF EXISTS(SELECT 1 FROM SYS.all_objects WHERE type = 'U' AND object_id = OBJECT_ID('[dbAuroraSA].[VentaDetalle]'))
+	DROP TABLE dbAuroraSA.VentaDetalle;
+GO
+
+IF EXISTS(SELECT 1 FROM SYS.all_objects WHERE type = 'U' AND object_id = OBJECT_ID('[dbAuroraSA].[Producto]'))
+	DROP TABLE dbAuroraSA.Producto;
+GO
+
+IF EXISTS(SELECT 1 FROM SYS.all_objects WHERE type = 'U' AND object_id = OBJECT_ID('[dbAuroraSA].[Venta]'))
+	DROP TABLE dbAuroraSA.Venta;
+GO
+
 IF EXISTS(SELECT 1 FROM SYS.all_objects WHERE type = 'U' AND object_id = OBJECT_ID('[dbAuroraSA].[Registro]'))
 	DROP TABLE dbAuroraSA.Registro;
 GO
@@ -143,6 +159,96 @@ CREATE TABLE dbAuroraSA.Catalogo(
 	)
 )
 GO
+
+CREATE TABLE dbAuroraSA.Producto(
+	idProducto			INT IDENTITY(1,1),
+	idCatalogo			INT,
+	nombre				VARCHAR(150) NOT NULL,
+	categoria			VARCHAR(100) NOT NULL,
+	precioUnitario		DECIMAL(10,2),
+	precioReferencia	DECIMAL(10,2),
+	unidadReferencia	VARCHAR(10),
+	proveedor			VARCHAR(20),
+	cantPorUnidad		INT,
+	activo				BIT DEFAULT 1,
+
+	CONSTRAINT PK_idProducto PRIMARY KEY (idProducto),
+
+	CONSTRAINT FK_idCatalogo FOREIGN KEY (idCatalogo)
+	REFERENCES dbAuroraSA.Catalogo(idCatalogo),
+
+	CONSTRAINT CK_cantPorUnidad CHECK(
+		cantPorUnidad > 0
+	)
+)
+GO
+
+CREATE TABLE dbAuroraSA.Venta(
+	idVenta				INT IDENTITY(1,1),
+	idFactura			CHAR(11) UNIQUE NOT NULL,
+	tipoFactura			CHAR(1) NOT NULL,
+	idCliente			INT NOT NULL,
+	idEmpleado			INT NOT NULL,
+	idSucursal			INT NOT NULL,
+	idMedioPago			INT NOT NULL,
+	identificaPago		CHAR(16),
+	fechaHora			DATETIME NOT NULL,
+	montoTotal			DECIMAL(10,2) NOT NULL,
+
+	CONSTRAINT PK_idVenta PRIMARY KEY (idVenta),
+
+	CONSTRAINT FK_idCliente FOREIGN KEY (idCliente)
+	REFERENCES dbAuroraSA.Cliente(idCliente),
+
+	CONSTRAINT FK_idEmpleado FOREIGN KEY (idEmpleado)
+	REFERENCES dbAuroraSA.Empleado(idEmpleado),
+
+	CONSTRAINT FK_idSucursal FOREIGN KEY (idSucursal)
+	REFERENCES dbAuroraSA.Sucursal(idSucursal),
+
+	CONSTRAINT FK_idMedioPago FOREIGN KEY (idMedioPago)
+	REFERENCES dbAuroraSA.MedioPago(idMedioPago),
+
+	CONSTRAINT CK_tipoFactura CHECK(
+		tipoFactura in ('A','B','C')
+	)
+)
+GO
+
+CREATE TABLE dbAuroraSA.VentaDetalle(
+	idVentaDetalle	INT IDENTITY(1,1),
+	idVenta			INT NOT NULL,
+	idProducto		INT NOT NULL,
+	cantidad		INT NOT NULL,
+	precioUnitario	DECIMAL(10,2) NOT NULL,
+
+	CONSTRAINT PK_idVentaDetalle PRIMARY KEY (idVentaDetalle),
+
+	CONSTRAINT FK_idVenta FOREIGN KEY (idVenta)
+	REFERENCES dbAuroraSA.Venta(idVenta),
+
+	CONSTRAINT FK_idProducto FOREIGN KEY (idProducto)
+	REFERENCES dbAuroraSA.Producto(idProducto)
+
+)
+GO
+
+CREATE TABLE dbAuroraSA.SucursalProducto(
+	idSucursalProducto	INT IDENTITY(1,1),
+	idSucursal			INT NOT NULL,
+	idProducto			INT NOT NULL,
+
+	CONSTRAINT PK_idSucursalProducto PRIMARY KEY (idSucursalProducto),
+
+	CONSTRAINT FK_idSucursal2 FOREIGN KEY (idSucursal)
+	REFERENCES dbAuroraSA.Venta(idVenta),
+
+	CONSTRAINT FK_idProducto2 FOREIGN KEY (idProducto)
+	REFERENCES dbAuroraSA.Producto(idProducto)
+)
+GO
+
+
 
 -- EJECUTAR POWERSHELL ".\ActualizarTC.ps1" PARA CARGAR TIPO DE CAMBIO BLUE
 
